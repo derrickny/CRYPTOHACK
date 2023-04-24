8d3e19ef399706c79d3dd984592a742538452662
Derrick Nyaga <nyagad@icloud.com>
2023-04-24 15:39:31 +0300

    Update You either know, XOR you don't.py

--- You either know, XOR you don't.py
+++ You either know, XOR you don't.py
@@ -8,7 +8,7 @@ Remember the flag format and how it might help you in this challenge!
 """
 from pwn import xor
 flag = bytes.fromhex('0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104')
-print(xor(flag, 'crypto{'.encode())) # oh, it says 'myXORke+y...'
-print(xor(flag, 'myXORkey'.encode())) # try this? yay, it works! sometimes simpler is better
+print(xor(flag, 'crypto{'.encode()))
+print(xor(flag, 'myXORkey'.encode())) 
 
 
