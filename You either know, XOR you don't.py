5eadbb06491987450f99df428b633a0d9f8b7ffb
nyagaderrick <nyagad@icloud.com>
2023-04-24 15:36:29 +0300

    Update You either know, XOR you don't.py

--- You either know, XOR you don't.py
+++ You either know, XOR you don't.py
@@ -6,27 +6,9 @@ Remember the flag format and how it might help you in this challenge!
 
 0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104
 """
-from binascii import unhexlify
-
-def brute(ciphertext, key):
-    if len(ciphertext) != len(key):
-        return "Failed!"
-    return bytes([a ^ b for a, b in zip(ciphertext, key)]).decode(errors="replace")
-
-ciphertext = unhexlify("0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104")
-print("[-] CIPHER: {}".format(ciphertext))
-
-# First Step
-partial_key = brute(ciphertext[:7], b"crypto{")
-print("[-] PARTIAL KEY FOUND: {}".format(partial_key))
-
-# Second Step
-key = (partial_key + "y").encode() * (len(ciphertext) // len(key)) + (partial_key + "y").encode()[:len(ciphertext) % len(key)]
-print("[-] Decoding using KEY: {}".format(key.decode()))
-
-# Final Step
-flag = brute(ciphertext, key)
-print("\n[*] FLAG: {}".format(flag))
-
+from pwn import xor
+flag = bytes.fromhex('0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104')
+print(xor(flag, 'crypto{'.encode())) # oh, it says 'myXORke+y...'
+print(xor(flag, 'myXORkey'.encode())) # try this? yay, it works! sometimes simpler is better
 
 
